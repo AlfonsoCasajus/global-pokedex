b5dc551083e807ccb2decd5d965643eaecf18097
AlfonsoCasajus <alfonso.casajus@hotmail.com>
2024-06-12 17:55:59 -0300

    welcome view

---
+++ src/components/utils/GButton.vue
@@ -0,0 +1,47 @@
+<template>
+  <button v-bind="$attrs">
+    <component :is="icon" v-if="icon" class="btn-icon" />
+    <div class="slot-wrapper">
+      <slot />
+    </div>
+  </button>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  props: {
+    icon: {
+      type: Object,
+      required: false,
+      default: () => null
+    }
+  }
+})
+</script>
+
+<style scoped>
+button {
+  .btn-icon {
+    width: 100%;
+    height: 100%;
+    align-self: center;
+    justify-self: center;
+  }
+
+  .slot-wrapper {
+    width: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+}
+
+button:disabled {
+  pointer-events: none;
+  cursor: default;
+  color: #ffffff;
+  opacity: 0.7;
+}
+</style>
---
+++ src/views/WelcomeView.vue
@@ -0,0 +1,41 @@
+<template>
+  <section>
+    <PikachuSvg />
+    <h1>Welcome to Pokédex</h1>
+    <p>
+      The digital encyclopedia created by Proffesor Oak is an invaluable tool to Trainers in the
+      Pokémon world
+    </p>
+    <div>
+      <GButton @click="$router.push({ name: 'pokedex' })">Get Started</GButton>
+    </div>
+  </section>
+</template>
+
+<script setup lang="ts">
+import PikachuSvg from '@/components/utils/PikachuSvg.vue'
+import GButton from '@/components/utils/GButton.vue'
+</script>
+
+<style scoped>
+section {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+  h1 {
+    font-weight: 700;
+    font-size: 26px;
+    line-height: 31.2px;
+  }
+
+  p {
+    max-width: 570px;
+    font-weight: 500;
+    font-size: 18px;
+    line-height: 27px;
+    text-align: center;
+  }
+}
+</style>
