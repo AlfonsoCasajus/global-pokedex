105df10a187b85cef44dc44d331ad5debccbffe8
AlfonsoCasajus <alfonso.casajus@hotmail.com>
2024-06-13 15:36:39 -0300

    pokedex view

---
+++ src/components/pokemons/PokemonDetail.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <img width="300px" src="@/assets/images/pokemon_background.png" alt="Pokemon backround image" />
+  </div>
+  <div>
+    <span>Name</span>
+    <span>Squirtle</span>
+  </div>
+  <div>
+    <span>Weight</span>
+    <span>20</span>
+  </div>
+  <div>
+    <span>Height</span>
+    <span>18</span>
+  </div>
+  <div>
+    <span>Types</span>
+    <span>Normal, Water</span>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
---
+++ src/views/PokedexView.vue
@@ -0,0 +1,40 @@
+<template>
+  <section v-if="true">
+    <PokemonsList />
+    <footer>
+      <span class="all-btn">
+        <GButton :icon="IconList" :active="true">All</GButton>
+      </span>
+      <span>
+        <GButton :icon="IconStarFilled" :active="false">Favorites</GButton>
+      </span>
+    </footer>
+  </section>
+  <GLoader v-else />
+</template>
+
+<script setup lang="ts">
+import PokemonsList from '@/components/pokemons/PokemonsList.vue'
+import GLoader from '@/components/utils/GLoader.vue'
+import GButton from '@/components/utils/GButton.vue'
+import { IconList, IconStarFilled } from '@tabler/icons-vue'
+</script>
+
+<style scoped>
+footer {
+  width: 100%;
+  height: 80px;
+  position: fixed;
+  bottom: 0;
+  left: 0;
+  background-color: #ffffff;
+  box-shadow: 0px -5px 4px 0px #0000000d;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  .all-btn {
+    margin-right: 15px;
+  }
+}
+</style>
